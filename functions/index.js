8955ffd2885051e911ff767c31b45f460771b792
zaptrem <zaptrem@me.com>
2019-09-24 05:28:53 +0000

    It works, but is limited to 18 minute clips. I'm too tired to remember what I did.

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,17 @@
-
-
-const functions = require('firebase-functions');
-const speech = require('@google-cloud/speech')
-const gcs = require('@google-cloud/storage');
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
-const ffmpeg = require('fluent-ffmpeg');
-const ffmpeg_static = require('ffmpeg-static');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const speech = require("@google-cloud/speech");
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+const ffmpeg = require("fluent-ffmpeg");
+const ffmpeg_static = require("ffmpeg-static");
+const admin = require("firebase-admin");
 admin.initializeApp();
-
+const gcs = admin.storage();
+const runtimeOpts = {
+  timeoutSeconds: 540,
+  memory: "512MB"
+};
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -18,28 +19,32 @@ admin.initializeApp();
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-exports.analyzeRecording = functions.firestore
-  .document('users/{userId}/recordings/{recordingId}')
+exports.analyzeRecording = functions
+  .region("us-east1")
+  .runWith(runtimeOpts)
+  .firestore.document("users/{userId}/recordings/{recordingId}")
   .onCreate(async (snap, context) => {
-       // Match to group, add to group list. Does the group have a transcription?
-       // did they reach the number for google transcription without an android phone?
+    // Match to group, add to group list. Does the group have a transcription?
+    // did they reach the number for google transcription without an android phone?
 
-       const recordingObject = snap.data();
-       let uri = recordingObject.uri;
-       let storageAddress = recordingObject.storageAddress;
-       let bucket = recordingObject.bucket;
-       let convertedURI = await convert(recordingObject)
+    const recordingObject = snap.data();
+    let uri = recordingObject.uri;
+    let storageAddress = recordingObject.storageAddress;
+    let bucket = recordingObject.bucket;
+    let convertedURI_pre = await convert(recordingObject);
+    let convertedURI = "gs://" + bucket + "/" + convertedURI_pre;
 
-       let result = await transcribe(convertedURI);
+    let result = await transcribe(convertedURI);
 
-
-       return change.after.ref.set({
+    return change.after.ref.set(
+      {
         transcription: result
-      }, {merge: true});
-});
+      },
+      { merge: true }
+    );
+  });
 // Obv transcribes the audio..
-async function transcribe(fileURI){
-
+async function transcribe(fileURI) {
   // Creates a client
   const client = new speech.SpeechClient();
 
@@ -47,42 +52,56 @@ async function transcribe(fileURI){
   const fileName = fileURI;
 
   // Reads a local audio file and converts it to base64
+  /*
   const file = fs.readFileSync(fileName);
   const audioBytes = file.toString('base64');
-
+  */
   // The audio file's encoding, sample rate in hertz, and BCP-47 language code
   const audio = {
-    content: audioBytes,
+    uri: fileName
   };
   const config = {
-    encoding: 'flac',
-    sampleRateHertz: 44000,
-    languageCode: 'en-US',
+    encoding: "flac",
+    sampleRateHertz: 44100,
+    languageCode: "en-US"
   };
   const request = {
     audio: audio,
-    config: config,
+    config: config
   };
 
+  console.log("starting transcriptino");
   // Detects speech in the audio file
-  const [response] = await client.recognize(request);
-  const transcription = response.results
-    .map(result => result.alternatives[0].transcript)
-    .join('\n');
-  console.log(`Transcription: ${transcription}`);
+  try {
+    const [operation] = await client.longRunningRecognize(request);
+    // Get a Promise representation of the final result of the job
+    console.log("sent transcript request");
+    const [response] = await operation.promise();
+    console.log("got response");
+    const transcription = response.results
+      .map(result => result.alternatives[0].transcript)
+      .join("\n");
+    console.log(`Transcription: ${transcription}`);
+  } catch (err) {
+    console.log("something went wrong: " + err);
+  }
 }
 
 // Makes an ffmpeg command return a promise.
 function promisifyCommand(command) {
   return new Promise((resolve, reject) => {
-    command.on('end', resolve).on('error', reject).run();
+    command
+      .on("end", resolve)
+      .on("error", reject)
+      .run();
   });
 }
 
 async function convert(object) {
-  const fileBucket = object.bucket; // The Storage bucket that contains the file.
-  const filePath = object.fileURI; // File path in the bucket.
-  const contentType = object.contentType; // File content type.
+  const fileBucket = object["bucket"]; // The Storage bucket that contains the file.
+  const filePath = object["storageAddress"]; // File path in the bucket.
+  const contentType = object["contentType"]; // File content type.
+  console.log(object);
 
   // Exit if this is triggered on a file that is not an audio.
   /*if (!contentType.startsWith('audio/')) {
@@ -93,8 +112,8 @@ async function convert(object) {
   // Get the file name.
   const fileName = path.basename(filePath);
   // Exit if the audio is already converted.
-  if (fileName.endsWith('_output.flac')) {
-    console.log('Already a converted audio.');
+  if (fileName.endsWith("_output.flac")) {
+    console.log("Already a converted audio.");
     return null;
   }
 
@@ -102,36 +121,39 @@ async function convert(object) {
   const bucket = gcs.bucket(fileBucket);
   const tempFilePath = path.join(os.tmpdir(), fileName);
   // We add a '_output.flac' suffix to target audio file name. That's where we'll upload the converted audio.
-  const targetTempFileName = fileName.replace(/\.[^/.]+$/, '') + '_output.flac';
+  const targetTempFileName = fileName.replace(/\.[^/.]+$/, "") + "_output.flac";
   const targetTempFilePath = path.join(os.tmpdir(), targetTempFileName);
-  const targetStorageFilePath = path.join(path.dirname(filePath), targetTempFileName);
+  const targetStorageFilePath = path.join(
+    path.dirname(filePath),
+    targetTempFileName
+  );
 
-  await bucket.file(filePath).download({destination: tempFilePath});
-  console.log('Audio downloaded locally to', tempFilePath);
+  await bucket.file(filePath).download({ destination: tempFilePath });
+  console.log("Audio downloaded locally to", tempFilePath);
   // Convert the audio to mono channel using FFMPEG.
 
   let command = ffmpeg(tempFilePath)
-      .setFfmpegPath(ffmpeg_static.path)
-      .audioChannels(1)
-      .audioFrequency(44000)
-      .format('flac')
-      .output(targetTempFilePath);
+    .setFfmpegPath(ffmpeg_static.path)
+    .audioChannels(1)
+    .audioFrequency(44100)
+    .format("flac")
+    .output(targetTempFilePath);
 
   await promisifyCommand(command);
-  console.log('Output audio created at', targetTempFilePath);
-  return targetTempFilePath;
+  console.log("Output audio created at", targetTempFilePath);
+  //return targetTempFilePath;
 
-  /*
   // Uploading the audio.
-  await bucket.upload(targetTempFilePath, {destination: targetStorageFilePath});
-  console.log('Output audio uploaded to', targetStorageFilePath);
+  const options = {
+    destination: targetStorageFilePath,
+    resumable: false
+  };
+  await bucket.upload(targetTempFilePath, options);
+  console.log("Output audio uploaded to", targetStorageFilePath);
 
   // Once the audio has been uploaded delete the local file to free up disk space.
   fs.unlinkSync(tempFilePath);
   fs.unlinkSync(targetTempFilePath);
-
-  return console.log('Temporary files removed.', targetTempFilePath);
-
-  */
+  console.log("Temporary files removed.", targetTempFilePath);
+  return targetStorageFilePath;
 }
-
