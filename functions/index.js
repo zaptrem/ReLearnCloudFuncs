8f61e8f6e2a2d61c0fe40f666f79837735bdc2c1
zaptrem <zaptrem@me.com>
2019-10-06 07:00:00 +0000

    Changed to async result fetching and funcs now don't transcribe when not needed.

--- functions/index.js
+++ functions/index.js
@@ -8,9 +8,15 @@ const ffmpeg_static = require("ffmpeg-static");
 const admin = require("firebase-admin");
 admin.initializeApp();
 const gcs = admin.storage();
+const db = admin.firestore();
+const express = require("express");
+const cookieParser = require("cookie-parser")();
+const cors = require("cors")({ origin: true });
+const app = express();
+const fetch = require("node-fetch");
 const runtimeOpts = {
   timeoutSeconds: 540,
-  memory: "512MB"
+  memory: "1GB"
 };
 
 // // Create and Deploy Your First Cloud Functions
@@ -23,20 +29,30 @@ exports.analyzeRecording = functions
   .region("us-east1")
   .runWith(runtimeOpts)
   .firestore.document("users/{userId}/recordings/{recordingId}")
-  .onCreate(async (snap, context) => {
+  .onWrite(async (change, context) => {
     // Match to group, add to group list. Does the group have a transcription?
     // did they reach the number for google transcription without an android phone?
 
-    const recordingObject = snap.data();
+    const document = change.after.exists ? change.after.data() : null;
+    if(document == null) {
+        print("document not found, likely deleted");
+        return;
+    }
+    const recordingObject = document;
     let uri = recordingObject.uri;
     let storageAddress = recordingObject.storageAddress;
     let bucket = recordingObject.bucket;
     let convertedURI_pre = await convert(recordingObject);
     let convertedURI = "gs://" + bucket + "/" + convertedURI_pre;
 
-    let result = await transcribe(convertedURI);
+    if (recordingObject.status != "NeedTranscription") return;
 
-    return snap.ref.set(
+    let result = await transcribe(convertedURI, change.after);
+
+
+
+    // Old server writes transcription code
+    return change.after.ref.set(
       {
         transcription: result
       },
@@ -44,7 +60,7 @@ exports.analyzeRecording = functions
     );
   });
 // Obv transcribes the audio..
-async function transcribe(fileURI) {
+async function transcribe(fileURI, snap) {
   // Creates a client
   const client = new speech.SpeechClient();
 
@@ -74,6 +90,14 @@ async function transcribe(fileURI) {
   // Detects speech in the audio file
   try {
     const [operation] = await client.longRunningRecognize(request);
+    snap.ref.set(
+      {
+        operation: operation.name,
+        status: "CloudTranscribing"
+      },
+      { merge: true }
+    );
+    /*
     // Get a Promise representation of the final result of the job
     console.log("sent transcript request");
     const [response] = await operation.promise();
@@ -83,9 +107,12 @@ async function transcribe(fileURI) {
       .join("\n");
     console.log(`Transcription: ${transcription}`);
     return transcription;
+    */
   } catch (err) {
     console.log("something went wrong: " + err);
   }
+
+  return;
 }
 
 // Makes an ffmpeg command return a promise.
@@ -158,3 +185,110 @@ async function convert(object) {
   console.log("Temporary files removed.", targetTempFilePath);
   return targetStorageFilePath;
 }
+
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+const validateFirebaseIdToken = async (req, res, next) => {
+  console.log("Check if request is authorized with Firebase ID token");
+
+  if (
+    (!req.headers.authorization ||
+      !req.headers.authorization.startsWith("Bearer ")) &&
+    !(req.cookies && req.cookies.__session)
+  ) {
+    console.error(
+      "No Firebase ID token was passed as a Bearer token in the Authorization header.",
+      "Make sure you authorize your request by providing the following HTTP header:",
+      "Authorization: Bearer <Firebase ID Token>",
+      'or by passing a "__session" cookie.'
+    );
+    res.status(403).send("Unauthorized");
+    return;
+  }
+
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    console.log('Found "Authorization" header');
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else if (req.cookies) {
+    console.log('Found "__session" cookie');
+    // Read the ID Token from cookie.
+    idToken = req.cookies.__session;
+  } else {
+    // No cookie
+    res.status(403).send("Unauthorized");
+    return;
+  }
+
+  try {
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+    console.log("ID Token correctly decoded", decodedIdToken);
+    req.user = decodedIdToken;
+    next();
+    return;
+  } catch (error) {
+    console.error("Error while verifying Firebase ID token:", error);
+    res.status(403).send("Unauthorized");
+    return;
+  }
+};
+
+// HTTPS CHECK FUNCTION, PARAMS: operation, id, uid(the user id)
+async function check(req, res) {
+    console.log("Starting check");
+  const operation = req.query.operation;
+  //const id = req.query.id;
+  //const uid = req.query.uid;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+
+  /*const snapshot = await admin
+    .database()
+    .ref("/messages")
+    .push({ original: original });*/
+  console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+  //let token = admin.credential.cert(JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)).getAccessToken;
+  let tokenObj = await admin.credential.applicationDefault().getAccessToken();
+  let token = tokenObj.access_token;
+  console.log(token);
+
+  const response = await fetch(
+    `https://speech.googleapis.com/v1/operations/${operation}`,
+    {
+      method: "GET",
+      headers: new fetch.Headers({
+        Authorization: "Bearer " + token,
+        Accept: "application/json"
+      })
+    }
+  );
+
+  /*let docRef = db
+    .collection("users")
+    .doc(uid)
+    .collection("recordings")
+    .doc(id);*/
+
+  //console.log(response);
+
+  res.send(response);
+}
+app.use(cors);
+app.use(cookieParser);
+app.use(validateFirebaseIdToken);
+app.get("/", (req, res) => check(req, res));
+
+
+// This HTTPS endpoint can only be accessed by your Firebase Users.
+// Requests need to be authorized by providing an `Authorization` HTTP header
+// with value `Bearer <Firebase ID Token>`.
+exports.checkRecording = functions.region("us-east1").https.onRequest(app);
